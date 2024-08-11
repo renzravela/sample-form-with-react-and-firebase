2f71e6e24cd47bf24a50a2781471c03b98c9c766
renz_aerol_ravela <renzaerol.ravela@gmail.com>
2024-08-11 21:41:24 +0800

    update

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -18,7 +18,7 @@ const Form = () => {
   }
 
   return (
-    <form ref={form} onSubmit={handleSubmit} className="w-6/12 grid grid-cols-1 gap-6 border-solid border-2 rounded p-5 shadow-lg">
+    <form ref={form} onSubmit={handleSubmit} className="myForm w-6/12 grid grid-cols-1 gap-6 border-solid border-2 rounded p-5 shadow-lg">
       <h1 className="text-3xl font-bold mb-5">Form Example</h1>
       <label className="block text-start">
         <span className="text-gray-500">Name</span>
